0c13a1da03542822c0dab333b18956c00f9ca974
adiharel <adi.harel10@gmail.com>
2023-11-16 17:31:53 +0200

    Working fetch of picture and showing it

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import express from 'express'
+import axios from 'axios';
+import express from 'express';
 
 const app = express();
 const port = 3000;
@@ -7,6 +8,18 @@ app.use(express.static("public"));
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
+});
+
+app.get('/get-image', async (req, res) => {
+    try {
+        const response = await axios.get('https://dog.ceo/api/breeds/image/random');
+        const imageUrl = response.data.message;
+        console.log('Image URL:', imageUrl); // Add this line
+        res.render('index.ejs', { url: imageUrl });
+    } catch (error) {
+        console.error('Error fetching image:', error); // Add this line
+        res.render('index.ejs', { error: error });
+    }
 });
 
 app.listen(port, () => {
